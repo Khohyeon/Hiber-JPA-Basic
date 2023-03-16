ee046beabce0b48f64bf686c0020ca7688e7d09c
khh5762 <khh5762@naver.com>
2023-03-16 16:26:32 +0900

    하이버네이트 이용하여 CRUD 완료

---
+++ src/main/java/shop/mtcoding/hiberapp/controller/UserApiController.java
@@ -0,0 +1,70 @@
+package shop.mtcoding.hiberapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.hiberapp.model.User;
+import shop.mtcoding.hiberapp.model.UserJpaRepository;
+import shop.mtcoding.hiberapp.model.UserRepository;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+    
+    // private final UserRepository userRepository;
+    private final UserJpaRepository userRepository;
+
+    @PostMapping("/users")
+    public ResponseEntity<?> addUser(User user) {
+        User userPS = userRepository.save(user);
+        return new ResponseEntity<>(userPS, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, User user) {
+        User userPS = userRepository.findById(id);
+        if(userPS == null){
+            return new ResponseEntity<>("해당 유저가 없습니다", HttpStatus.BAD_REQUEST);
+        }
+        userPS.update(user.getPassword(), user.getEmail());
+        User updateUserPS = userRepository.update(userPS);
+        return new ResponseEntity<>(updateUserPS, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id) {
+        User userPS = userRepository.findById(id);
+        if(userPS == null){
+            return new ResponseEntity<>("해당 유저가 없습니다", HttpStatus.BAD_REQUEST);
+        }
+        userRepository.delete(userPS);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> findUsers(@RequestParam(defaultValue = "0") int page) {
+        List<User> userListPS = userRepository.findAll(page, 2);
+        return new ResponseEntity<>(userListPS, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> findUsers(@PathVariable Long id) {
+        User userPS = userRepository.findById(id);
+        if(userPS == null) {
+            return new ResponseEntity<>("해당 유저가 없습니다", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(userPS, HttpStatus.OK);
+    }
+
+}
